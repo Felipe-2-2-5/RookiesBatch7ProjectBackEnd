f77bfe657e26513ed2b339f9b8c7dc71b4ddea25
Dang Phan Thanh Cong <dangphanthanhcong.work@gmail.com>
2024-06-19 15:03:04 +0700

    fix test

--- Backend.Tests/ServiceTests/UserServiceTests.cs
+++ Backend.Tests/ServiceTests/UserServiceTests.cs
@@ -36,38 +36,38 @@ namespace Backend.Tests.ServiceTests
         [Test]
         public async Task GetByIdAsync_WhenUserExists_ReturnsUserResponse()
         {
-            Arrange
-           var userId = 1;
+            // Arrange
+            var userId = 1;
             var user = new User { Id = userId };
             var userResponse = new UserResponse { Id = userId };
 
             _userRepoMock.Setup(repo => repo.GetByIdAsync(userId)).ReturnsAsync(user);
             _mapperMock.Setup(mapper => mapper.Map<UserResponse>(user)).Returns(userResponse);
 
-            Act
-           var result = await _userService.GetByIdAsync(userId);
+            // Act
+            var result = await _userService.GetByIdAsync(userId);
 
-            Assert
-             Assert.That(result, Is.EqualTo(userResponse));
+            // Assert
+            Assert.That(result, Is.EqualTo(userResponse));
         }
 
         [Test]
         public void GetByIdAsync_WhenUserDoesNotExist_ThrowsNotFoundException()
         {
-            Arrange
-           var userId = 1;
+            // Arrange
+            var userId = 1;
 
             _userRepoMock.Setup(repo => repo.GetByIdAsync(userId)).ReturnsAsync((User)null);
 
-            Act & Assert
-             Assert.ThrowsAsync<NotFoundException>(() => _userService.GetByIdAsync(userId));
+            // Act & Assert
+            Assert.ThrowsAsync<NotFoundException>(() => _userService.GetByIdAsync(userId));
         }
 
         [Test]
         public async Task InsertAsync_WhenUserIsValid_ReturnsUserResponse()
         {
-            Arrange
-           var userDto = new UserDTO();
+            // Arrange
+            var userDto = new UserDTO();
             var validationResult = new ValidationResult();
             var user = new User();
             var userResponse = new UserResponse();
@@ -79,21 +79,21 @@ namespace Backend.Tests.ServiceTests
             _userRepoMock.Setup(repo => repo.FindUserByUserNameAsync(user.UserName)).ReturnsAsync(user);
             _mapperMock.Setup(mapper => mapper.Map<UserResponse>(user)).Returns(userResponse);
 
-            Act
-           var result = await _userService.InsertAsync(userDto);
+            // Act
+            var result = await _userService.InsertAsync(userDto, "abc");
 
-            Assert
-             Assert.That(result, Is.EqualTo(userResponse));
+            // Assert
+            Assert.That(result, Is.EqualTo(userResponse));
         }
 
         [Test]
         public void InsertAsync_WhenUserIsInvalid_ThrowsDataInvalidException()
         {
-            Arrange
-           var userDto = new UserDTO();
+            // Arrange
+            var userDto = new UserDTO();
             var validationResult = new ValidationResult(new List<ValidationFailure> {
-                 new ValidationFailure("PropertyName", "Error message")
-             });
+                new ValidationFailure("PropertyName", "Error message")
+            });
 
             _validatorMock.Setup(validator => validator.ValidateAsync(userDto, default)).ReturnsAsync(validationResult);
 
@@ -104,49 +104,49 @@ namespace Backend.Tests.ServiceTests
         [Test]
         public async Task ChangePasswordAsync_WhenOldPasswordIsCorrect_ChangesPassword()
         {
-            Arrange
-           var changePasswordDto = new ChangePasswordDTO { Id = 1, OldPassword = "oldPass", NewPassword = "NewPass123!" };
+            // Arrange
+            var changePasswordDto = new ChangePasswordDTO { Id = 1, OldPassword = "oldPass", NewPassword = "NewPass123!" };
             var user = new User { Id = 1, Password = "oldPass" };
 
             _userRepoMock.Setup(repo => repo.GetByIdAsync(changePasswordDto.Id)).ReturnsAsync(user);
 
-            Act
-           var result = await _userService.ChangePasswordAsync(changePasswordDto);
+            // Act
+            var result = await _userService.ChangePasswordAsync(changePasswordDto);
 
-            Assert
-             Assert.That(result, Is.True);
+            // Assert
+            Assert.That(result, Is.True);
             Assert.That(user.Password, Is.EqualTo(changePasswordDto.NewPassword));
         }
 
         [Test]
         public void ChangePasswordAsync_WhenOldPasswordIsIncorrect_ThrowsDataInvalidException()
         {
-            Arrange
-           var changePasswordDto = new ChangePasswordDTO { Id = 1, OldPassword = "wrongPass", NewPassword = "NewPass123!" };
+            // Arrange
+            var changePasswordDto = new ChangePasswordDTO { Id = 1, OldPassword = "wrongPass", NewPassword = "NewPass123!" };
             var user = new User { Id = 1, Password = "oldPass" };
 
             _userRepoMock.Setup(repo => repo.GetByIdAsync(changePasswordDto.Id)).ReturnsAsync(user);
 
-            Act & Assert
-             Assert.ThrowsAsync<DataInvalidException>(() => _userService.ChangePasswordAsync(changePasswordDto));
+            // Act & Assert
+            Assert.ThrowsAsync<DataInvalidException>(() => _userService.ChangePasswordAsync(changePasswordDto));
         }
 
         [Test]
         public async Task LoginAsync_WhenCredentialsAreCorrect_ReturnsLoginResponse()
         {
-            Arrange
-           var loginDto = new LoginDTO { UserName = "user", Password = "password" };
+            // Arrange
+            var loginDto = new LoginDTO { UserName = "user", Password = "password" };
             var user = new User { UserName = "user", Password = "password" };
             var token = "token";
 
             _userRepoMock.Setup(repo => repo.FindUserByUserNameAsync(loginDto.UserName)).ReturnsAsync(user);
             _tokenServiceMock.Setup(tokenService => tokenService.GenerateJWTWithUser(It.IsAny<User>(), It.IsAny<IEnumerable<Claim>?>())).Returns(token);
 
-            Act
-           var result = await _userService.LoginAsync(loginDto);
+            // Act
+            var result = await _userService.LoginAsync(loginDto);
 
-            Assert
-             Assert.That(result.Flag, Is.True);
+            // Assert
+            Assert.That(result.Flag, Is.True);
             Assert.That(result.Message, Is.EqualTo("Login success"));
             Assert.That(result.Token, Is.EqualTo(token));
         }
@@ -154,25 +154,25 @@ namespace Backend.Tests.ServiceTests
         [Test]
         public async Task LoginAsync_WhenCredentialsAreIncorrect_ReturnsLoginResponse()
         {
-            Arrange
-           var loginDto = new LoginDTO { UserName = "user", Password = "wrongPassword" };
+            // Arrange
+            var loginDto = new LoginDTO { UserName = "user", Password = "wrongPassword" };
             var user = new User { UserName = "user", Password = "password" };
 
             _userRepoMock.Setup(repo => repo.FindUserByUserNameAsync(loginDto.UserName)).ReturnsAsync(user);
 
-            Act
-           var result = await _userService.LoginAsync(loginDto);
+            // Act
+            var result = await _userService.LoginAsync(loginDto);
 
-            Assert
-             Assert.That(result.Flag, Is.False);
+            // Assert
+            Assert.That(result.Flag, Is.False);
             Assert.That(result.Message, Is.EqualTo("Invalid credentials"));
         }
 
         [Test]
         public async Task GetFilterAsync_ReturnsPaginationResponse()
         {
-            Arrange
-           var filterRequest = new UserFilterRequest();
+            // Arrange
+            var filterRequest = new UserFilterRequest();
             var users = new List<User> { new User() };
             var paginationResponse = new PaginationResponse<User>(users, 1);
             var userResponses = new List<UserResponse> { new UserResponse() };
@@ -180,11 +180,11 @@ namespace Backend.Tests.ServiceTests
             _userRepoMock.Setup(repo => repo.GetFilterAsync(filterRequest, Location.HaNoi)).ReturnsAsync(paginationResponse);
             _mapperMock.Setup(mapper => mapper.Map<IEnumerable<UserResponse>>(users)).Returns(userResponses);
 
-            Act
-           var result = await _userService.GetFilterAsync(filterRequest, Location.HaNoi);
+            // Act
+            var result = await _userService.GetFilterAsync(filterRequest, Location.HaNoi);
 
-            Assert
-             Assert.That(result.Data, Is.EqualTo(userResponses));
+            // Assert
+            Assert.That(result.Data, Is.EqualTo(userResponses));
             Assert.That(result.TotalCount, Is.EqualTo(1));
         }
 
